8a5efca89901dc956999267fd0eef1e809feded3
Feiping Huang <feiping.huang@yottaa.com>
2015-01-08 18:57:11 +0800

    add gzip

--- controllers/gziped.rb
+++ controllers/gziped.rb
@@ -4,6 +4,8 @@ require 'nokogiri'
 class Gziped < Goliath::API
   def response(env)
     path = env["PATH_INFO"]
-    [200, {}, {response: FileSystem.new(path).get}]
+    env.stream_send FileSystem.new(path).get
+    env.stream_close
+    [200, {}, Goliath::Response::STREAMING]
   end
 end
