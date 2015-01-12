e1d29876b61537b7714f22dc3dc2be26b9cddfd5
Feiping Huang <feiping.huang@yottaa.com>
2015-01-12 11:54:50 +0800

    add request_count

--- server.rb
+++ server.rb
@@ -1,18 +1,30 @@
 require 'goliath'
-require 'nokogiri'
+require 'tilt'
 require "zlib"
 require 'rack'
 require 'stringio'
+require 'erb'
 
 class Server < Goliath::API
   # use ::Rack::Deflater, if: proc { |env| env["PATH_INFO"] =~ /^\/gziped\// }
+
+  use(Rack::Static,                     # render static files from ./public
+      :root => Goliath::Application.app_path("public"),
+      :urls => ["/favicon.ico", '/stylesheets', '/javascripts', '/images'])
+
+  @@request_count = Hash.new(0)
+  @@current_path = "/"
+
+  def request_count
+    (@@request_count[@@current_path] += 1) % 10
+  end
 
   def on_close(env)
     env.logger.info "Connection closed."
   end
 
   def response(env)
-    case env["PATH_INFO"]
+    case @@current_path = env["PATH_INFO"]
     when /^\/chunked\/(.*\.html)/
       handle(views_path($1)) { |path| chunk(env, path) }
     when /^\/gziped\/(.*\.html)/
@@ -21,18 +33,19 @@ class Server < Goliath::API
       handle(views_path($1)) { |path| gzip_chunk(env, path) }
     when /^\/(.*\.html)/
       handle(views_path($1)) { |path| ordinary(env, path) }
+    else
+      raise Goliath::Validation::NotFoundError
     end
   end
 
   def ordinary(env, path)
-    [200, {}, [File.read(path)]]
+    [200, {}, [render(path)]]
   end
 
   def chunk(env, path)
     operation = proc do
-      split_html(File.read(path)).each do |chunk|
+      split_html(render(path)).each do |chunk|
         env.chunked_stream_send(chunk)
-        sleep 0.5
       end
     end
 
@@ -52,11 +65,10 @@ class Server < Goliath::API
     gzip = Zlib::GzipWriter.new(io)
 
     operation = proc do
-      split_html(File.read(path)).each do |chunk|
+      split_html(render(path)).each do |chunk|
         env.chunked_stream_send(compress(gzip, io, chunk))
         io.truncate(0)
         io.rewind
-        sleep 0.5
       end
       gzip.close
     end
@@ -79,13 +91,19 @@ class Server < Goliath::API
     gzip = Zlib::GzipWriter.new(io)
     headers = { 'Content-Type' => 'text/html', 'Content-Encoding' => "gzip", 'X-Stream' => 'Goliath' }
 
-    [200, headers, [compress(gzip, io, File.read(path))]]
+    [200, headers, [compress(gzip, io, render(path))]]
   ensure
     gzip.close
   end
 
   def views_path(path)
     File.dirname(__FILE__) + '/views/' + path
+  end
+
+  def render(path, options={})
+    data = File.read(path)
+    template = Tilt['erb'].new(nil, nil, {}){ data }
+    template.render self, options
   end
 
   def compress(gzip, io, data)
